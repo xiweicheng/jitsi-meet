f08cad08b5743517adc1408c1e8ed662afd2f59d
hristoterezov <hristo@jitsi.org>
2018-09-24 19:08:55 -0500

    feat(dropbox): For mobile. This involves redesign of the web recording dialog in order to look the same as the mobile one.

---
+++ react/features/recording/components/Recording/styles.web.js
@@ -0,0 +1,4 @@
+// We are using styles only for mobile and css for web. Export empty object so
+// that all styles[*] statements on web won't trigger an error and will be
+// undefined.
+export default {};
